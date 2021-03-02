1c9cbda148847863c80136de0a1bc4d098298e5c
AbdurRahmanG <abdurrahmangofficial@gmail.com>
2021-03-02 21:42:12 +0530

    All Things Done Like Firebase Connection and Ads

--- app/src/main/java/com/techilyfly/tictactoe/MainActivity.java
+++ app/src/main/java/com/techilyfly/tictactoe/MainActivity.java
@@ -10,6 +10,14 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
 public class MainActivity extends AppCompatActivity {
 
     ImageView imageViewButton1, imageViewButton2, imageViewButton3, imageViewButton4, imageViewButton5, imageViewButton6, imageViewButton7, imageViewButton8, imageViewButton9;
@@ -18,10 +26,60 @@ public class MainActivity extends AppCompatActivity {
     private TextView scoreX, scoreO;
     private Button reset;
 
+    private AdView mAdView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Ads Initialization will begins here.
+        // MobileAds.initialize(this);
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        mAdView = findViewById(R.id.adView);
+        mAdView = findViewById(R.id.adViewTop);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        mAdView.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+            }
+
+            @Override
+            public void onAdFailedToLoad(LoadAdError adError) {
+                // Code to be executed when an ad request fails.
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+            }
+
+            @Override
+            public void onAdClicked() {
+                // Code to be executed when the user clicks on an ad.
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                // Code to be executed when the user has left the app.
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when the user is about to return
+                // to the app after tapping on an ad.
+            }
+        });
+
 
         imageViewButton1 = findViewById(R.id.imageViewButton1);
         imageViewButton2 = findViewById(R.id.imageViewButton2);
